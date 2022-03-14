8a4cd0eb0d9545157bc804b2cb70a93333f1190c
Amir Hossein Sarebani <amirsarebani1381@gmail.com>
2022-03-14 23:44:24 +0330

    Update testcases

--- unique_ptr_test.cpp
+++ unique_ptr_test.cpp
@@ -9,61 +9,56 @@ TEST(UniquePtrTest, DefaultConstructor) {
 
 // 2
 TEST(UniquePtrTest, Constructor) {
-    int i = 10;
-    unique_ptr<int> u_ptr(&i);
-    ASSERT_EQ(u_ptr.get(), &i);
+    int *ptr = new int(10);
+    unique_ptr<int> u_ptr(ptr);
+    ASSERT_EQ(u_ptr.get(), ptr);
 }
 
 // 3
 TEST(UniquePtrTest, MoveCostructor) {
-    int i = 10;
-    unique_ptr<int> u_ptr1(&i);
-    ASSERT_EQ(u_ptr1.get(), &i);
+    unique_ptr<int> u_ptr1(new int(10));
+    ASSERT_EQ(*u_ptr1.get(), 10);
     unique_ptr<int> u_ptr2(std::move(u_ptr1));
     ASSERT_EQ(u_ptr1.get(), nullptr);
-    ASSERT_EQ(u_ptr2.get(), &i);
+    ASSERT_EQ(*u_ptr2.get(), 10);
 }
 
 // 4
 TEST(UniquePtrTest, ReleaseTest) {
-    int i = 10;
-    unique_ptr<int> u_ptr(&i);
-    ASSERT_EQ(u_ptr.release(), &i);
+    int *ptr = new int(10);
+    unique_ptr<int> u_ptr(ptr);
+    ASSERT_EQ(u_ptr.release(), ptr);
     ASSERT_FALSE(u_ptr);
+    delete ptr;
 }
 
 // 5
 TEST(UniquePtrTest, ResetTest) {
-    int i = 10;
-    int j = 20;
-    unique_ptr<int> u_ptr(&i);
+    unique_ptr<int> u_ptr(new int(10));
     u_ptr.reset();
     ASSERT_EQ(u_ptr.get(), nullptr);
-    u_ptr.reset(&j);
-    ASSERT_EQ(u_ptr.get(), &j);
+    u_ptr.reset(new int(20));
+    ASSERT_EQ(*u_ptr.get(), 20);
 }
 
 // 6
 TEST(UniquePtrTest, MoveAssign) {
-    int i = 10;
-    unique_ptr<int> u_ptr1(&i);
+    unique_ptr<int> u_ptr1(new int(10));
     unique_ptr<int> u_ptr2 = std::move(u_ptr1);
     ASSERT_EQ(u_ptr1.get(), nullptr);
-    ASSERT_EQ(u_ptr2.get(), &i);
+    ASSERT_EQ(*u_ptr2.get(), 10);
 }
 
 // 7
 TEST(UniquePtrTest, BoolConversion) {
-    int i = 10;
     unique_ptr<int> u_ptr1;
-    unique_ptr<int> u_ptr2(&i);
+    unique_ptr<int> u_ptr2(new int(10));
     ASSERT_FALSE(u_ptr1);
-    ASSERT_FALSE(!u_ptr2);
+    ASSERT_TRUE(u_ptr2);
 }
 
 // 8
 TEST(UniquePtrTest, StarOperator) {
-    int i = 10;
-    unique_ptr<int> u_ptr(&i);
+    unique_ptr<int> u_ptr(new int(10));
     ASSERT_EQ(*u_ptr, 10);
 }
