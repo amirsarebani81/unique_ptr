6c5bb7ecf3536080229925dd50daf1845f77c638
Amir Hossein Sarebani <amirsarebani1381@gmail.com>
2022-03-15 15:49:39 +0330

    Add array support to unique_ptr

--- unique_ptr.hpp
+++ unique_ptr.hpp
@@ -2,6 +2,7 @@
 #define UNIQUE_PTR
 
 #include <algorithm>
+#include <cstddef>
 
 template <typename T> class unique_ptr {
   private:
@@ -44,13 +45,61 @@ template <typename T> class unique_ptr {
         return *this;
     }
 
-    bool operator==(const unique_ptr &u_ptr) { return pointer == u_ptr.pointer; }
+    bool operator==(const unique_ptr &u_ptr) const { return pointer == u_ptr.pointer; }
 
-    bool operator!=(const unique_ptr &u_ptr) { return !operator==(u_ptr); }
+    bool operator!=(const unique_ptr &u_ptr) const { return pointer != u_ptr.pointer; }
 
     T &operator*() { return *pointer; }
 
     T *operator->() { return pointer; }
+};
+
+template <typename T> class unique_ptr<T[]> {
+  private:
+    T *pointer;
+
+  public:
+    unique_ptr()
+        : pointer(nullptr) {}
+
+    unique_ptr(T ptr[])
+        : pointer(ptr) {}
+
+    unique_ptr(unique_ptr &&u_ptr)
+        : pointer(u_ptr.pointer) {
+        u_ptr.pointer = nullptr;
+    }
+
+    ~unique_ptr() { delete[] pointer; }
+
+    T *release() {
+        T *tmp = pointer;
+        pointer = nullptr;
+        return tmp;
+    }
+
+    void reset(T *ptr = nullptr) {
+        delete[] pointer;
+        pointer = ptr;
+    }
+
+    void swap(unique_ptr &u_ptr) { std::swap(pointer, u_ptr.pointer); }
+
+    T *get() { return pointer; }
+
+    operator bool() const { return pointer != nullptr; }
+
+    unique_ptr &operator=(unique_ptr &&u_ptr) {
+        pointer = u_ptr.pointer;
+        u_ptr.pointer = nullptr;
+        return *this;
+    }
+
+    bool operator==(const unique_ptr &u_ptr) const { return pointer == u_ptr.pointer; }
+
+    bool operator!=(const unique_ptr &u_ptr) const { return pointer != u_ptr.pointer; }
+
+    T &operator[](const std::size_t index) { return pointer[index]; }
 };
 
 #endif
