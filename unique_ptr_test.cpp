bc4363667d47d03cbaaee93a960d4ed360e6f403
Amir Hossein Sarebani <amirsarebani1381@gmail.com>
2022-03-15 15:49:47 +0330

    Update testcases

--- unique_ptr_test.cpp
+++ unique_ptr_test.cpp
@@ -1,64 +1,132 @@
 #include "unique_ptr.hpp"
 #include <gtest/gtest.h>
 
-// 1
-TEST(UniquePtrTest, DefaultConstructor) {
+TEST(PrimaryTemplateTest, DefaultConstructor) {
     unique_ptr<int> u_ptr;
     ASSERT_EQ(u_ptr.get(), nullptr);
 }
 
-// 2
-TEST(UniquePtrTest, Constructor) {
-    int *ptr = new int(10);
-    unique_ptr<int> u_ptr(ptr);
-    ASSERT_EQ(u_ptr.get(), ptr);
+TEST(PrimaryTemplateTest, Constructor) {
+    unique_ptr<int> u_ptr(new int);
+    ASSERT_NE(u_ptr.get(), nullptr);
 }
 
-// 3
-TEST(UniquePtrTest, MoveCostructor) {
-    unique_ptr<int> u_ptr1(new int(10));
-    ASSERT_EQ(*u_ptr1.get(), 10);
+TEST(PrimaryTemplateTest, MoveCostructor) {
+    unique_ptr<int> u_ptr1(new int);
+    ASSERT_NE(u_ptr1.get(), nullptr);
     unique_ptr<int> u_ptr2(std::move(u_ptr1));
     ASSERT_EQ(u_ptr1.get(), nullptr);
-    ASSERT_EQ(*u_ptr2.get(), 10);
+    ASSERT_NE(u_ptr2.get(), nullptr);
 }
 
-// 4
-TEST(UniquePtrTest, ReleaseTest) {
-    int *ptr = new int(10);
-    unique_ptr<int> u_ptr(ptr);
-    ASSERT_EQ(u_ptr.release(), ptr);
-    ASSERT_FALSE(u_ptr);
+TEST(PrimaryTemplateTest, ReleaseTest) {
+    unique_ptr<int> u_ptr(new int);
+    int *ptr = u_ptr.release();
+    ASSERT_EQ(u_ptr.get(), nullptr);
     delete ptr;
 }
 
-// 5
-TEST(UniquePtrTest, ResetTest) {
-    unique_ptr<int> u_ptr(new int(10));
+TEST(PrimaryTemplateTest, ResetTest) {
+    unique_ptr<int> u_ptr(new int);
     u_ptr.reset();
     ASSERT_EQ(u_ptr.get(), nullptr);
-    u_ptr.reset(new int(20));
-    ASSERT_EQ(*u_ptr.get(), 20);
+    u_ptr.reset(new int);
+    ASSERT_NE(u_ptr.get(), nullptr);
 }
 
-// 6
-TEST(UniquePtrTest, MoveAssign) {
+TEST(PrimaryTemplateTest, SwapTest) {
     unique_ptr<int> u_ptr1(new int(10));
-    unique_ptr<int> u_ptr2 = std::move(u_ptr1);
-    ASSERT_EQ(u_ptr1.get(), nullptr);
+    unique_ptr<int> u_ptr2(new int(20));
+    u_ptr1.swap(u_ptr2);
+    ASSERT_EQ(*u_ptr1.get(), 20);
     ASSERT_EQ(*u_ptr2.get(), 10);
 }
 
-// 7
-TEST(UniquePtrTest, BoolConversion) {
+TEST(PrimaryTemplateTest, MoveAssign) {
+    unique_ptr<int> u_ptr1(new int);
+    unique_ptr<int> u_ptr2 = std::move(u_ptr1);
+    ASSERT_EQ(u_ptr1.get(), nullptr);
+    ASSERT_NE(u_ptr2.get(), nullptr);
+}
+
+TEST(PrimaryTemplateTest, BoolConversion) {
     unique_ptr<int> u_ptr1;
-    unique_ptr<int> u_ptr2(new int(10));
+    unique_ptr<int> u_ptr2(new int);
     ASSERT_FALSE(u_ptr1);
     ASSERT_TRUE(u_ptr2);
 }
 
-// 8
-TEST(UniquePtrTest, StarOperator) {
+TEST(PrimaryTemplateTest, StarOperator) {
     unique_ptr<int> u_ptr(new int(10));
     ASSERT_EQ(*u_ptr, 10);
+}
+
+TEST(PrimaryTemplateTest, EqualOperator) {
+    unique_ptr<int> u_ptr1;
+    unique_ptr<int> u_ptr2(new int);
+    ASSERT_NE(u_ptr1, u_ptr2);
+}
+
+TEST(SpecializationForArrays, Constructor) {
+    unique_ptr<int[]> u_ptr(new int[10]);
+    ASSERT_NE(u_ptr.get(), nullptr);
+}
+
+TEST(SpecializationForArrays, MoveCostructor) {
+    unique_ptr<int[]> u_ptr1(new int[10]);
+    ASSERT_NE(u_ptr1.get(), nullptr);
+    unique_ptr<int[]> u_ptr2(std::move(u_ptr1));
+    ASSERT_EQ(u_ptr1.get(), nullptr);
+    ASSERT_NE(u_ptr2.get(), nullptr);
+}
+
+TEST(SpecializationForArrays, ReleaseTest) {
+    unique_ptr<int[]> u_ptr(new int[10]);
+    int *arr = u_ptr.release();
+    ASSERT_EQ(u_ptr.get(), nullptr);
+    delete[] arr;
+}
+
+TEST(SpecializationForArrays, ResetTest) {
+    unique_ptr<int[]> u_ptr(new int[10]);
+    u_ptr.reset();
+    ASSERT_EQ(u_ptr.get(), nullptr);
+    u_ptr.reset(new int[10]);
+    ASSERT_NE(u_ptr.get(), nullptr);
+}
+
+TEST(SpecializationForArrays, SwapTest) {
+    unique_ptr<int[]> u_ptr1(new int[10]);
+    unique_ptr<int[]> u_ptr2;
+    u_ptr1.swap(u_ptr2);
+    ASSERT_EQ(u_ptr1.get(), nullptr);
+    ASSERT_NE(u_ptr2.get(), nullptr);
+}
+
+TEST(SpecializationForArrays, MoveAssign) {
+    unique_ptr<int[]> u_ptr1(new int[10]);
+    unique_ptr<int[]> u_ptr2 = std::move(u_ptr1);
+    ASSERT_EQ(u_ptr1.get(), nullptr);
+    ASSERT_NE(u_ptr2.get(), nullptr);
+}
+
+TEST(SpecializationForArrays, BoolConversion) {
+    unique_ptr<int[]> u_ptr1;
+    unique_ptr<int[]> u_ptr2(new int[10]);
+    ASSERT_FALSE(u_ptr1);
+    ASSERT_TRUE(u_ptr2);
+}
+
+TEST(SpecializationForArrays, BracketOperator) {
+    unique_ptr<int[]> u_ptr(new int[10]);
+    for (size_t i = 0; i < 10; i++)
+        u_ptr[i] = i * i;
+    for (size_t i = 0; i < 10; i++)
+        ASSERT_EQ(u_ptr[i], i * i);
+}
+
+TEST(SpecializationForArrays, EqualOperator) {
+    unique_ptr<int[]> u_ptr1;
+    unique_ptr<int[]> u_ptr2(new int[10]);
+    ASSERT_NE(u_ptr1, u_ptr2);
 }
